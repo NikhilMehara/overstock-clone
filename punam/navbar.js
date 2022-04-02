95eb6d2a36c9d509cc42aafb2e1b3413dca5136f
punam <jagtappunam19@gmail.com>
2022-04-02 13:07:01 +0530

    updated

---
+++ punam/navbar.js
@@ -0,0 +1,175 @@
+var navbar = () =>{
+    return`    <nav>
+    <div class="navbar">
+       <div class="logo">
+        <img src="https://logos-download.com/wp-content/uploads/2017/01/Overstock_logo.png" class="logo" alt="">
+      
+       </div>
+        <!-- logo-->
+       <!--Search-->
+        <div class="search-box" >
+         <input type="text" class="search-bar" placeholder="search"   >
+       <!--search button-->
+        <button   class="search-btn"><img src="https://cdn-icons-png.flaticon.com/512/622/622669.png" width="20px" height="20px" onClick="get()"></button>
+      </div>
+      <div class="user-options">
+        <figure id="id001">
+          <img src="https://cdn-icons-png.flaticon.com/512/747/747376.png"  width="30px" height="30px" class="icon" alt="">
+          <figcaption>Acount</figcaption>
+        </figure>
+        <figure id="id002">
+          <img src="https://cdn-icons-png.flaticon.com/512/860/860808.png"  width="30px" height="30px" class="icon" alt="">
+          <figcaption>List</figcaption>
+        </figure>
+        <figure id="id003">
+          <img src="https://cdn-icons-png.flaticon.com/512/1170/1170678.png"  width="30px" height="30px" class="icon" alt="">
+          <figcaption>Cart</figcaption>
+        </figure> 
+        
+      </div>
+     
+      </div>
+      <div class="nav">
+        <ul>
+            <li><a href="#"  >Furniture</a> 
+                <ul>
+                    <li><a href="#" >Sofas & Couches</a></li>
+                    <li><a href="#" >Sectionals</a></li>
+                    <li><a href="#" >Benches </a></li> 
+                    <li><a href="#" >Ottomans & Poufs</a></li>
+                    <li><a href="#" >Accent Chairs</a></li>
+                    <li><a href="#" >Recliners</a></li>
+                </ul> </li>
+                <li> <a href="#" >Rugs</a>
+                    
+                    <ul>
+                        <li><a href="#" >Sofas & Couches</a></li>
+                        <li><a href="#" >Ideas</a></li>
+                        <li><a href="#" >Sales </a></li>
+                        <li><a href="#" >Sofas & Couches</a></li>
+                        <li><a href="#" >Ideas</a></li>
+                        <li><a href="#" >Sales </a></li> 
+                    </ul> </li>
+                <li> <a href="#" >Decor</a><ul>
+                    <li><a href="#" >Wall Mirrors </a></li>
+                    <li><a href="#" >Bathroom Vanity Mirrors</a></li>
+                    <li><a href="#" >Floor Mirrors </a></li>
+                    <li><a href="#" > Full Length Mirrors</a></li>  
+                    <li><a href="#" > Rectangular Mirrors </a></li> 
+                    <li><a href="#" > Round Mirrors </a></li> 
+                    <li><a href="#" >Window Mirror </a></li> 
+                </ul></li>
+                <li> <a href="#" >Outdoor</a><ul>
+                    <li><a href="#" >Sofas & Couches</a></li>
+                    <li><a href="#" >Ideas</a></li>
+                    <li><a href="#" >Sales </a></li> 
+                </ul></li>
+                <li> <a href="#" >Jewelry</a>
+                    <ul>
+                        <li><a href="#" >Sofas & Couches</a></li>
+                        <li><a href="#" >Ideas</a></li>
+                        <li><a href="#" >Sales </a></li> 
+                    </ul> </li>
+             
+                <li><a href="#" >Deales </a>
+                    <ul>
+                        <li><a href="#" >Sofas & Couches</a></li>
+                        <li><a href="#" >Ideas</a></li>
+                        <li><a href="#" >Sales </a></li> 
+                    </ul> </li>
+                <li><a href="#" >Kitchen</a>
+                    <ul>
+                    <li><a href="#" >Dinnerware Sets</a></li>
+                    <li><a href="#" >Formal Dinnerware</a></li>
+                    <li><a href="#" >Plates</a></li>
+                    <li><a href="#" >Bowls</a></li>
+                    <li><a href="#" >Cups & Saucers</a></li>
+                    <li><a href="#" >Mugs</a></li>  
+                </ul> </li> 
+                <li> <a href="#" >Outdoor</a>
+                    <ul>
+                    <li><a href="#" >Patio Furniture Sets</a></li>
+                    <li><a href="#" >Sofas, Chairs & Sectionals</a></li>
+                    <li><a href="#" >Dining Sets</a></li>
+                    <li><a href="#" >Dining Tables</a></li>
+                    <li><a href="#" >Dining Chairs</a></li>
+                    <li><a href="#" >Coffee & Side Tables</a></li>  
+                </ul> </li>
+                <li> <a href="#" >Jewelry</a>
+                    <ul>
+                    <li><a href="#" >Rings</a></li>
+                    <li><a href="#" >Necklaces</a></li>
+                    <li><a href="#" >Earrings</a></li>
+                    <li><a href="#" >Bracelets</a></li>
+                    <li><a href="#" >Moissanite Rings</a></li>
+                    <li><a href="#" >Jewelry Boxes</a></li>  
+                </ul> </li>
+                 <li> <a href="#" >Lighting</a>
+                    <ul>
+                    <li><a href="#" >Chandeliers</a></li>
+                    <li><a href="#" >Pendant Lights</a></li>
+                    <li><a href="#" >Flush Mount Lights</a></li>
+                    <li><a href="#" >Recessed Lights</a></li>
+                    <li><a href="#" >Semi-Flush Mount Lights</a></li>
+                    <li><a href="#" >Wagon Wheel Chandeliers</a></li>  
+                </ul> </li>
+                <li> <a href="#" >Kids</a>
+                    <ul>
+                    <li><a href="#" >Kids Beds</a></li>
+                    <li><a href="#" >Kids Mattresses</a></li>
+                    <li><a href="#" >Kids Dressers</a></li>
+                    <li><a href="#" >Kids Desks</a></li>
+                    <li><a href="#" >Kids Storage & Toy Boxes</a></li>
+                    <li><a href="#" >Bean Bags</a></li>  
+                </ul> </li>
+                <li><a href="#" >More</a>
+                    <ul>
+                    <li><a href="#" >Gifts for Her</a></li>
+                    <li><a href="#" >Gifts for Him</a></li>
+                    <li><a href="#" >Gifts for Kids & Baby</a></li>
+                    <li><a href="#" >Gifts for Foodies</a></li>
+                    <li><a href="#" >Gifts for Plant Lovers</a></li>
+                    <li><a href="#" >Gifts for Newlyweds</a></li>  
+                </ul> </li>
+                <li><a href="#" >Ideas</a>
+                    <ul>
+                    <li><a href="#" >Sofas & Couches</a></li>
+                    <li><a href="#" >Ideas</a></li>
+                    <li><a href="#" >Sales </a></li> 
+                </ul> </li>
+                <li><a href="#" >Sales </a>
+                    <ul>
+                    <li><a href="#" >Sofas & Couches</a></li>
+                    <li><a href="#" >Ideas</a></li>
+                    <li><a href="#" >Sales </a></li> 
+                </ul> </li>
+                <li><a href="#" style="color: red; ">  Deals </a></li>      
+    </ul>
+   
+    </div>
+</nav>`
+}
+
+
+
+var navbar_data = () => {
+    let button = document.getElementById("btn")
+    let account= document.getElementById("id001")
+    let list = document.getElementById("id002")
+    let cart = document.getElementById("id003")
+  
+    account.addEventListener("click",()=>{
+      window.location.href="./login.html"
+    })
+  
+     list.addEventListener("click", () => {
+        window.location.href = "./list.html"
+      })
+  
+       cart.addEventListener("click", () => {
+          window.location.href = "./cart.html"
+        })
+        button.addEventListener("click", () => {
+          window.locaation.href = "./seeall-style.html"
+        })
+}
